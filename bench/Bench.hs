631274e14f6ec27a63da68ce0978c1216e76a874
rubenpieters <rubenpietersgithub@gmail.com>
2018-07-16 12:29:51 +0200

    clean up benchmark file

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -28,20 +28,61 @@ import           Data.Function ((&))
 
 import qualified Data.List as L
 
-import           System.Environment (getArgs)
+-------------------------------------------------
+-- main
+-------------------------------------------------
 
-sumN :: Int
-sumN = 1000000
+main :: IO ()
+main = do
+  print ("basicResult " ++ show basicResult)
+  print ("sumResult " ++ show sumResult)
+  print ("primesResult " ++ show primesResult)
+  defaultMain
+      [ bgroup "length-io"   [ bench "streaming"      $ nfIOf streaming_basic basicN
+                             , bench "conduit"        $ nfIOf conduit_basic basicN
+                             , bench "pipes"          $ nfIOf pipes_basic basicN
+                             , bench "iostreams"      $ nfIOf iostreams_basic basicN
+                             , bench "machine"        $ nfIOf machines_basic basicN
+                             , bench "vector"         $ nfIOf vector_basic basicN
+                             , bench "orth-pipes"     $ nfIOf orthpipes_basic basicN
+                             ]
+      , bgroup "length-id"   [ bench "streaming"      $ nf (runIdentity . streaming_basic) basicN
+                             , bench "conduit"        $ nf (runIdentity . conduit_basic) basicN
+                             , bench "pipes"          $ nf (runIdentity . pipes_basic) basicN
+                             , bench "machine"        $ nf (runIdentity . machines_basic) basicN
+                             , bench "vector"         $ nf (runIdentity . vector_basic) basicN
+                             , bench "orth-pipes"     $ nf (runIdentity . orthpipes_basic) basicN
+                             ]
+      , bgroup "sum-io"      [ bench "streaming"      $ nfIOf streaming_sum sumN
+                             , bench "conduit"        $ nfIOf conduit_sum sumN
+                             , bench "pipes"          $ nfIOf pipes_sum sumN
+                             , bench "iostreams"      $ nfIOf iostreams_sum sumN
+                             , bench "machine"        $ nfIOf machines_sum sumN
+                             , bench "vector"         $ nfIOf vector_sum sumN
+                             ]
+      , bgroup "sum-id"      [ bench "streaming"      $ nf (runIdentity . streaming_sum) sumN
+                             , bench "conduit"        $ nf (runIdentity . conduit_sum) sumN
+                             , bench "pipes"          $ nf (runIdentity . pipes_sum) sumN
+                             , bench "machine"        $ nf (runIdentity . machines_sum) sumN
+                             , bench "vector"         $ nf (runIdentity . vector_sum) sumN
+                             ]
+      , bgroup "primes-io"   [ bench "streaming"      $ nfIOf streaming_primes primesN
+                             , bench "vector"         $ nfIOf vector_primes primesN
+                             , bench "conduit"        $ nfIOf conduit_primes primesN
+                             , bench "pipes"          $ nfIOf pipes_primes primesN
+                             , bench "orth-pipes"     $ nfIOf orthpipes_primes primesN
+                             ]
+      , bgroup "primes-id"   [ bench "streaming"      $ nf (runIdentity . streaming_primes) primesN
+                             , bench "vector"         $ nf (runIdentity . vector_primes) primesN
+                             , bench "conduit"        $ nf (runIdentity . conduit_primes) primesN
+                             , bench "pipes"          $ nf (runIdentity . pipes_primes) primesN
+                             , bench "orth-pipes"     $ nf (runIdentity . orthpipes_primes) primesN
+                             ]
+      ]
 
-sumResult :: Int
-sumResult = L.foldl' (+) 0 $
-    [1..sumN]
-  & Prelude.filter even
-  & Prelude.map (+1)
-  & Prelude.drop 1000
-  & Prelude.map (+1)
-  & Prelude.filter (\x -> x `mod` 2 == 0)
-
+-------------------------------------------------
+-- length benchmark
+-------------------------------------------------
 
 basicN :: Int
 basicN = 10000
@@ -55,80 +96,18 @@ basicResult = Prelude.length $
   & Prelude.map (+1)
   & Prelude.filter (\x -> x `mod` 2 == 0)
 
-primesN :: Int
-primesN = 10000
-
-sieveL :: [Int] -> [Int]
-sieveL (p:xs) = p : sieveL (Prelude.filter (\ x -> x `mod` p /= 0) xs)
-sieveL [] = []
-
-primesResult :: Int
-primesResult = Prelude.last $ Prelude.take primesN $ sieveL [2..]
-
-main :: IO ()
-main = timeMain
-
-timeMain :: IO ()
-timeMain = do
-  name : n : _ <- getArgs
-  runBench name (read n)
-
--- (/usr/bin/time -f %U stack bench --benchmark-arguments 'primes-orthpipes 10000')
-
-runBench :: String -> Int -> IO ()
-runBench "primes-orthpipes" n = orthpipes_primes n >>= print
-runBench "primes-streaming" n = streaming_primes n >>= print
-
-criterionMain :: IO ()
-criterionMain = do
-  print ("basicResult " ++ show basicResult)
-  print ("sumResult " ++ show sumResult)
-  print ("primesResult " ++ show primesResult)
-  defaultMain
-      [ bgroup "sum"      [ bench "streaming"      $ nfIOf streaming_sum sumN
-                          , bench "conduit"        $ nfIOf conduit_sum sumN
-                          , bench "pipes"          $ nfIOf pipes_sum sumN
-                          , bench "iostreams"      $ nfIOf iostreams_sum sumN
-                          , bench "machine"        $ nfIOf machines_sum sumN
-                          , bench "vector"         $ nfIOf vector_sum sumN
-                          ]
-      {-, bgroup "toList" [ bench "streaming"      $ nfIOf streaming_basic basicN
-                          , bench "conduit"        $ nfIOf conduit_basic basicN
-                          , bench "pipes"          $ nfIOf pipes_basic basicN
-                          , bench "iostreams"      $ nfIOf iostreams_basic basicN
-                          , bench "machine"        $ nfIOf machines_basic basicN
-                          , bench "vector"         $ nfIOf vector_basic basicN
-                          , bench "orth-pipes"     $ nfIOf orthpipes_basic basicN
-                          ]-}
-       {-, bgroup "primes"   [ bench "orth-pipes"     $ nfIOf orthpipes_primes primesN
-                          , bench "streaming"      $ nfIOf streaming_primes primesN
-                          ]-}
-      ]
-
-pipes_basic :: Int -> IO Int
-pipes_basic to = do
-  xs <- P.toListM $
-              P.each [1..to]
-          >-> P.filter even
-          >-> P.map (+1)
-          >-> P.drop 1000
-          >-> P.map (+1)
-          >-> P.filter (\x -> x `mod` 2 == 0)
+streaming_basic :: (Monad m) => Int -> m Int
+streaming_basic to = do
+  xs <- Str.toList_ $
+            Str.each [1..to]
+          & Str.filter even
+          & Str.map (+1)
+          & Str.drop 1000
+          & Str.map (+1)
+          & Str.filter (\x -> x `mod` 2 == 0)
   return $ checkBasic (Prelude.length xs)
 
-
-pipes_sum :: Int -> IO Int
-pipes_sum to = do
-  n <- P.fold (+) 0 id $
-             P.each [1..to]
-         >-> P.filter even
-         >-> P.map (+1)
-         >-> P.drop 1000
-         >-> P.map (+1)
-         >-> P.filter (\x -> x `mod` 2 == 0)
-  return $ checkSum n
-
-conduit_basic :: Int -> IO Int
+conduit_basic :: (Monad m) => Int -> m Int
 conduit_basic to = do
   xs <- runConduit $
              C.yieldMany [1..to]
@@ -141,7 +120,90 @@ conduit_basic to = do
           )
   return $ checkBasic (Prelude.length xs)
 
-conduit_sum :: Int -> IO Int
+pipes_basic :: (Monad m) => Int -> m Int
+pipes_basic to = do
+  xs <- P.toListM $
+              P.each [1..to]
+          >-> P.filter even
+          >-> P.map (+1)
+          >-> P.drop 1000
+          >-> P.map (+1)
+          >-> P.filter (\x -> x `mod` 2 == 0)
+  return $ checkBasic (Prelude.length xs)
+
+iostreams_basic :: Int -> IO Int
+iostreams_basic to = do
+  s0 <- IOS.fromList [1..to]
+  s1 <- IOS.filter even s0
+  s2 <- IOS.map (+1) s1
+  s3 <- IOS.drop 1000 s2
+  s4 <- IOS.map (+1) s3
+  s5 <- IOS.filter (\x -> x `mod` 2 == 0) s4
+  xs <- IOS.toList s5
+  assert (Prelude.length xs == basicResult) $
+      return (Prelude.length (xs :: [Int]))
+
+machines_basic :: (Monad m) => Int -> m Int
+machines_basic to = do
+  xs <- M.runT $
+               M.source [1..to :: Int]
+          M.~> M.filtered even
+          M.~> M.mapping (+1)
+          M.~> M.dropping 1000
+          M.~> M.mapping (+1)
+          M.~> M.filtered (\x -> x `mod` 2 == 0)
+  return $ checkBasic (Prelude.length xs)
+
+vector_basic :: (Monad m) => Int -> m Int
+vector_basic to = do
+  xs <- V.toList
+           $ V.filter even
+           $ V.map (+1)
+           $ V.drop 1000
+           $ V.map (+1)
+           $ V.filter (\x -> x `mod` 2 == 0)
+           $ V.enumFromTo 1 to
+  return $ checkBasic (Prelude.length xs)
+
+orthpipes_basic :: (Monad m) => Int -> m Int
+orthpipes_basic to = do
+  xs <- fetchResponses $ O.construct $
+                O.each [1..to]
+          O.>-> O.filter even
+          O.>-> O.map (+1)
+          O.>-> O.drop 1000
+          O.>-> O.map (+1)
+          O.>-> O.filter (\x -> x `mod` 2 == 0)
+  return $ checkBasic (Prelude.length xs)
+
+-------------------------------------------------
+-- sum benchmark
+-------------------------------------------------
+
+sumN :: Int
+sumN = 1000000
+
+sumResult :: Int
+sumResult = L.foldl' (+) 0 $
+    [1..sumN]
+  & Prelude.filter even
+  & Prelude.map (+1)
+  & Prelude.drop 1000
+  & Prelude.map (+1)
+  & Prelude.filter (\x -> x `mod` 2 == 0)
+
+streaming_sum :: (Monad m) => Int -> m Int
+streaming_sum to = do
+  n <- Str.sum_ $
+           Str.each [1..to]
+         & Str.filter even
+         & Str.map (+1)
+         & Str.drop 1000
+         & Str.map (+1)
+         & Str.filter (\x -> x `mod` 2 == 0)
+  return $ checkSum n
+
+conduit_sum :: (Monad m) => Int -> m Int
 conduit_sum to = do
   n <- runConduit $
                C.yieldMany [1..to]
@@ -154,26 +216,15 @@ conduit_sum to = do
             )
   return $ checkSum n
 
-streaming_basic :: Int -> IO Int
-streaming_basic to = do
-  xs <- Str.toList_ $
-            Str.each [1..to]
-          & Str.filter even
-          & Str.map (+1)
-          & Str.drop 1000
-          & Str.map (+1)
-          & Str.filter (\x -> x `mod` 2 == 0)
-  return $ checkBasic (Prelude.length xs)
-
-streaming_sum :: Int -> IO Int
-streaming_sum to = do
-  n <- Str.sum_ $
-           Str.each [1..to]
-         & Str.filter even
-         & Str.map (+1)
-         & Str.drop 1000
-         & Str.map (+1)
-         & Str.filter (\x -> x `mod` 2 == 0)
+pipes_sum :: (Monad m) => Int -> m Int
+pipes_sum to = do
+  n <- P.fold (+) 0 id $
+             P.each [1..to]
+         >-> P.filter even
+         >-> P.map (+1)
+         >-> P.drop 1000
+         >-> P.map (+1)
+         >-> P.filter (\x -> x `mod` 2 == 0)
   return $ checkSum n
 
 iostreams_sum :: Int -> IO Int
@@ -188,33 +239,7 @@ iostreams_sum to = do
   assert (n == sumResult) $
       return n
 
-iostreams_basic :: Int -> IO Int
-iostreams_basic to = do
-  s0 <- IOS.fromList [1..to]
-  s1 <- IOS.filter even s0
-  s2 <- IOS.map (+1) s1
-  s3 <- IOS.drop 1000 s2
-  s4 <- IOS.map (+1) s3
-  s5 <- IOS.filter (\x -> x `mod` 2 == 0) s4
-  xs <- IOS.toList s5
-  assert (Prelude.length xs == basicResult) $
-      return (Prelude.length (xs :: [Int]))
-
-
-
-
-machines_basic :: Int -> IO Int
-machines_basic to = do
-  xs <- M.runT $
-               M.source [1..to :: Int]
-          M.~> M.filtered even
-          M.~> M.mapping (+1)
-          M.~> M.dropping 1000
-          M.~> M.mapping (+1)
-          M.~> M.filtered (\x -> x `mod` 2 == 0)
-  return $ checkBasic (Prelude.length xs)
-
-machines_sum :: Int -> IO Int
+machines_sum :: (Monad m) => Int -> m Int
 machines_sum to = do
   n <- foldlT (+) 0 $
               M.source [1..to :: Int]
@@ -225,18 +250,7 @@ machines_sum to = do
          M.~> M.filtered (\x -> x `mod` 2 == 0)
   return $ checkSum n
 
-vector_basic :: Int -> IO Int
-vector_basic to = do
-  xs <- V.toList
-           $ V.filter even
-           $ V.map (+1)
-           $ V.drop 1000
-           $ V.map (+1)
-           $ V.filter (\x -> x `mod` 2 == 0)
-           $ V.enumFromTo 1 to
-  return $ checkBasic (Prelude.length xs)
-
-vector_sum :: Int -> IO Int
+vector_sum :: (Monad m) => Int -> m Int
 vector_sum to = do
   n <- V.foldl (+) 0
           $ V.filter even
@@ -247,24 +261,19 @@ vector_sum to = do
           $ V.enumFromTo 1 to
   return $ checkSum n
 
-orthpipes_basic :: Int -> IO Int
-orthpipes_basic to = do
-  xs <- fetchResponses $ O.construct $
-                O.each [1..to]
-          O.>-> O.filter even
-          O.>-> O.map (+1)
-          O.>-> O.drop 1000
-          O.>-> O.map (+1)
-          O.>-> O.filter (\x -> x `mod` 2 == 0)
-  return $ checkBasic (Prelude.length xs)
+-------------------------------------------------
+-- pipes benchmark
+-------------------------------------------------
 
-orthpipes_primes :: Int -> IO Int
-orthpipes_primes n = do
-  ps <- fetchResponses $ O.construct $
-                O.upfrom 2
-          O.>-> O.sieve
-          O.>-> O.take n
-  return $ checkPrimes (Prelude.last ps)
+primesN :: Int
+primesN = 10000
+
+list_sieve :: [Int] -> [Int]
+list_sieve (p:xs) = p : list_sieve (Prelude.filter (\x -> x `mod` p /= 0) xs)
+list_sieve [] = []
+
+primesResult :: Int
+primesResult = Prelude.last $ Prelude.take primesN $ list_sieve [2..]
 
 streaming_sieve :: (Monad m) => Stream (Of Int) m () -> Stream (Of Int) m ()
 streaming_sieve str = case str of
@@ -272,12 +281,68 @@ streaming_sieve str = case str of
       Effect m -> Effect (fmap streaming_sieve m)
       Return _ -> return ()
 
-streaming_primes :: Int -> IO Int
+streaming_primes :: (Monad m) => Int -> m Int
 streaming_primes n = do
   ps <- Str.toList_ $
             Str.enumFrom 2
           & streaming_sieve
           & Str.take n
+  return $ checkPrimes (Prelude.last ps)
+
+conduit_sieve :: (Monad m) => ConduitT Int Int m ()
+conduit_sieve = do
+  mP <- C.await
+  case mP of
+    Just p -> do
+      C.yield p
+      C.filter (\x -> x `mod` p /= 0) .| conduit_sieve
+    Nothing -> return ()
+
+conduit_primes :: (Monad m) => Int -> m Int
+conduit_primes n = do
+  xs <- runConduit $
+             C.yieldMany [2..]
+          .| conduit_sieve
+          .| C.take n
+          .| C.sinkList
+  return $ checkPrimes (Prelude.last xs)
+
+pipes_sieve :: (Monad m) => Pipe Int Int m x
+pipes_sieve = do
+  p <- P.await
+  P.yield p
+  P.filter (\x -> x `mod` p /= 0) >-> pipes_sieve
+
+pipes_primes :: (Monad m) => Int -> m Int
+pipes_primes n = do
+  xs <- P.toListM $
+              P.each [2..]
+          >-> pipes_sieve
+          >-> P.take n
+  return $ checkPrimes (Prelude.last xs)
+
+vector_sieve :: (Monad m) => V.Stream m Int -> m (V.Stream m Int)
+vector_sieve str = do
+  p <- V.head str
+  let xs = V.tail str
+  let predicate x = x `mod` p /= 0
+  nextPrimes <- vector_sieve (V.filter predicate xs)
+  return $ p `V.cons` nextPrimes
+
+vector_primes :: (Monad m) => Int -> m Int
+vector_primes n = do
+  ps' <-   V.fromList [2..]
+         & vector_sieve
+  ps <-    V.take n ps'
+         & V.toList
+  return $ checkPrimes (Prelude.last ps)
+
+orthpipes_primes :: (Monad m) => Int -> m Int
+orthpipes_primes n = do
+  ps <- fetchResponses $ O.construct $
+                O.upfrom 2
+          O.>-> O.sieve
+          O.>-> O.take n
   return $ checkPrimes (Prelude.last ps)
 
 -------------------------------------------------
